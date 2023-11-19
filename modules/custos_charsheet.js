08c309956e7e62dec91be6a22b6a3f78d91bba7e
ParvusDomus <parvusdomus@gmail.com>
2023-11-19 14:12:58 +0100

    curriculum page, life and pietas improv. encumbran

--- modules/custos_charsheet.js
+++ modules/custos_charsheet.js
@@ -16,6 +16,7 @@ export default class CUSTOS_CHAR_SHEET extends ActorSheet{
         this._prepareCharacterItems(data);
         //this._updateInitiative(data);
         this._calculateResources(data);
+        this._calculateXp(data);
         if (this.actor.system.creatio.locked == false){
           this._updateProvintiaValues(data);
           this._setAgeBonus(data);
@@ -35,26 +36,39 @@ export default class CUSTOS_CHAR_SHEET extends ActorSheet{
       const Talent = [];
       const Rituals = [];
       const Summonings = [];
+      let weight=0;
       for (let i of sheetData.items){
         switch (i.type){
 				  case 'weapon':
 				  {
             Weapons.push(i);
+            if (i.system.equipped !== 'dropped'){
+              weight+=i.system.weight;
+            }
             break;
 				  }
           case 'armor':
 				  {
             Armors.push(i);
+            if (i.system.equipped !== 'dropped'){
+              weight+=i.system.weight;
+            }            
             break;
 				  }
           case 'shield':
 				  {
             Shields.push(i);
+            if (i.system.equipped !== 'dropped'){
+              weight+=i.system.weight;
+            }            
             break;
 				  }
           case 'object':
 				  {
             Objects.push(i);
+            if (i.system.equipped !== 'dropped'){
+              weight+=i.system.weight;
+            }            
             break;
 				  }
           case 'provintia':
@@ -100,6 +114,9 @@ export default class CUSTOS_CHAR_SHEET extends ActorSheet{
       actorData.Talent = Talent;
       actorData.Rituals = Rituals;
       actorData.Summonings = Summonings;
+      this.actor.update ({ 'system.total_encumbrance': weight });
+      let converted = Number (this.actor.system.resources.life.max) - weight
+      this.actor.update ({ 'system.converted_encumbrance': converted });
       actorData.settings = {
 
       }
@@ -263,6 +280,53 @@ export default class CUSTOS_CHAR_SHEET extends ActorSheet{
       let pietas=Number(this.actor.system.virtutes.ratio.value)+Number(this.actor.system.virtutes.sensibilitas.value)+Number(pietas_office_bonus)+Number(this.actor.system.pietas_xp_bonus)
       this.actor.update ({ 'system.resources.life.max': life });
       this.actor.update ({ 'system.resources.pietas.max': pietas });
+    }
+
+    _calculateXp(sheetData){
+      const actorData = sheetData;
+      let total=0
+      let remaining=0
+      //DE BELLO
+      total=Number(this.actor.system.curriculum.experience)*Number(this.actor.system.curriculum.bello.multiplier)
+      this.actor.update ({ 'system.curriculum.bello.total': total });
+      remaining=Number(this.actor.system.curriculum.bello.total)-Number(this.actor.system.curriculum.bello.spent)
+      this.actor.update ({ 'system.curriculum.bello.remaining': remaining });
+      //DE CORPORE
+      total=Number(this.actor.system.curriculum.experience)*Number(this.actor.system.curriculum.corpore.multiplier)
+      this.actor.update ({ 'system.curriculum.corpore.total': total });
+      remaining=Number(this.actor.system.curriculum.corpore.total)-Number(this.actor.system.curriculum.corpore.spent)
+      this.actor.update ({ 'system.curriculum.corpore.remaining': remaining });
+      //DE MAGIA
+      total=Number(this.actor.system.curriculum.experience)*Number(this.actor.system.curriculum.magia.multiplier)
+      this.actor.update ({ 'system.curriculum.magia.total': total });
+      remaining=Number(this.actor.system.curriculum.magia.total)-Number(this.actor.system.curriculum.magia.spent)
+      this.actor.update ({ 'system.curriculum.magia.remaining': remaining });
+      //DE NATURA
+      total=Number(this.actor.system.curriculum.experience)*Number(this.actor.system.curriculum.natura.multiplier)
+      this.actor.update ({ 'system.curriculum.natura.total': total });
+      remaining=Number(this.actor.system.curriculum.natura.total)-Number(this.actor.system.curriculum.natura.spent)
+      this.actor.update ({ 'system.curriculum.natura.remaining': remaining });
+      //DE SCIENTIA
+      total=Number(this.actor.system.curriculum.experience)*Number(this.actor.system.curriculum.scientia.multiplier)
+      this.actor.update ({ 'system.curriculum.scientia.total': total });
+      remaining=Number(this.actor.system.curriculum.scientia.total)-Number(this.actor.system.curriculum.scientia.spent)
+      this.actor.update ({ 'system.curriculum.scientia.remaining': remaining });
+      //DE SOCIETATE
+      total=Number(this.actor.system.curriculum.experience)*Number(this.actor.system.curriculum.societate.multiplier)
+      this.actor.update ({ 'system.curriculum.societate.total': total });
+      remaining=Number(this.actor.system.curriculum.societate.total)-Number(this.actor.system.curriculum.societate.spent)
+      this.actor.update ({ 'system.curriculum.societate.remaining': remaining });
+      //ARCANORUM
+      total=Number(this.actor.system.curriculum.experience)*Number(this.actor.system.curriculum.arcanorum.multiplier)
+      this.actor.update ({ 'system.curriculum.arcanorum.total': total });
+      remaining=Number(this.actor.system.curriculum.arcanorum.total)-Number(this.actor.system.curriculum.arcanorum.spent)
+      this.actor.update ({ 'system.curriculum.arcanorum.remaining': remaining });
+      //DEORUM
+      total=Number(this.actor.system.curriculum.experience)*Number(this.actor.system.curriculum.deorum.multiplier)
+      this.actor.update ({ 'system.curriculum.deorum.total': total });
+      remaining=Number(this.actor.system.curriculum.deorum.total)-Number(this.actor.system.curriculum.deorum.spent)
+      this.actor.update ({ 'system.curriculum.deorum.remaining': remaining });
+
     }
 
 
